c0ade0d4237a2aa759229474b4b9b22ba6f46890
atc0m <atcom21@gmail.com>
2024-12-01 23:29:53 +0000

    part 2 done

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 
-fn parse_input_part1(contents: &str) -> (Vec<i32>, Vec<i32>) {
+fn parse_columns(contents: &str) -> (Vec<i32>, Vec<i32>) {
     let mut column1: Vec<i32> = Vec::new();
     let mut column2: Vec<i32> = Vec::new();
     for line in contents.lines() {
@@ -17,7 +18,7 @@ fn parse_input_part1(contents: &str) -> (Vec<i32>, Vec<i32>) {
 
 fn part1(contents: &str) {
     println!("Part 1");
-    let (mut column1, mut column2) = parse_input_part1(&contents);
+    let (mut column1, mut column2) = parse_columns(&contents);
     column1.sort();
     column2.sort();
 
@@ -30,8 +31,27 @@ fn part1(contents: &str) {
     println!("Sum of absolute differences: {}", sum);
 }
 
+fn count_numbers_in_column(column: &Vec<i32>) -> HashMap<i32, i32> {
+    let mut frequency: HashMap<i32, i32> = HashMap::new();
+    for num in column {
+        let count = frequency.entry(*num).or_insert(0);
+        *count += 1;
+    }
+    frequency
+}
+
 fn part2(contents: &str) {
     println!("Part 2");
+    let (column1, column2) = parse_columns(&contents);
+    
+    let frequency2 = count_numbers_in_column(&column2);
+    println!("Frequencies of column 2: {:?}", frequency2);
+
+    let mut sum_of_multiples = 0;
+    for num in &column1 {
+        sum_of_multiples += num * frequency2.get(&num).copied().unwrap_or(0);
+    }
+    println!("Sum of multiples: {}", sum_of_multiples);
 }
 
 fn main() {
